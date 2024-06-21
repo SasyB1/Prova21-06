3c6e13740492559ac7c1bce9361051a6d627dbc4
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-06-21 12:55:37 +0200

    Aggiungere i file di progetto.

---
+++ Prova21-06/Contribuente.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova21_06
+{
+    // Enumerazione per rappresentare il genere
+    public enum Sesso
+    {
+        Uomo,
+        Donna
+    }
+
+    // Classe che rappresenta un Contribuente
+    public class Contribuente
+    {
+        // Proprietà della classe Contribuente
+        public string nome { get; set; }         
+        public string cognome { get; set; }         
+        public Sesso genere { get; set; }           
+        public DateOnly dataNascita { get; set; }    
+        public string codiceFiscale { get; set; }   
+        public string comuneResidenza { get; set; }   
+        public double redditoAnnuale { get; set; }     
+
+        // Costruttore per inizializzare l'oggetto Contribuente
+        public Contribuente(string nome, string cognome, Sesso genere, DateOnly dataNascita, string comuneResidenza, double redditoAnnuale, string codiceFiscale)
+        {
+            // Inizializza le proprietà con i valori passati
+            this.nome = nome;
+            this.cognome = cognome;
+            this.genere = genere;
+            this.dataNascita = dataNascita;
+            this.comuneResidenza = comuneResidenza;
+            this.redditoAnnuale = redditoAnnuale;
+            this.codiceFiscale = codiceFiscale;
+        }
+
+        // Metodo per calcolare l'imposta basata sul reddito annuale
+        public double CalcoloImposta()
+        {
+            double imposta = 0;
+
+            // Calcola l'imposta in base agli scaglioni di reddito
+            if (redditoAnnuale <= 15000)
+            {
+                imposta = redditoAnnuale * 0.23;
+            }
+            else if (redditoAnnuale > 15000 && redditoAnnuale <= 28000)
+            {
+                imposta = 3450 + (redditoAnnuale - 15000) * 0.27;
+            }
+            else if (redditoAnnuale > 28000 && redditoAnnuale <= 55000)
+            {
+                imposta = 6960 + (redditoAnnuale - 28000) * 0.38;
+            }
+            else if (redditoAnnuale > 55000 && redditoAnnuale <= 75000)
+            {
+                imposta = 17220 + (redditoAnnuale - 55000) * 0.41;
+            }
+            else if (redditoAnnuale > 75000)
+            {
+                imposta = 25420 + (redditoAnnuale - 75000) * 0.43;
+            }
+
+            return imposta; // Restituisce l'imposta calcolata
+        }
+    }
+}
---
+++ Prova21-06/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace Prova21_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Altrimenti non mi funziona il simbolo dell'euro €
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Richiesta all'utente di inserire i dati del contribuente
+            Console.WriteLine("Inserisci i dati del contribuente");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Cognome: ");
+            string cognome = Console.ReadLine();
+            Console.Write("Genere Uomo/Donna (U/D): ");
+            string inputGenere = Console.ReadLine().ToUpper();
+            Sesso genere;
+
+            // Confrontiamo l'input dell'utente per determinare il genere
+            if (inputGenere == "U")
+            {
+                genere = Sesso.Uomo;
+            }
+            else if (inputGenere == "D")
+            {
+                genere = Sesso.Donna;
+            }
+            else
+            {
+                Console.WriteLine("Input non valido."); 
+                return; 
+            }
+
+            // Richiesta all'utente di inserire ulteriori informazioni
+            Console.Write("Data di nascita (gg/mm/aaaa): ");
+            DateOnly dataNascita = DateOnly.Parse(Console.ReadLine());
+            Console.Write("Comune di residenza: ");
+            string comuneResidenza = Console.ReadLine();
+            Console.WriteLine("Codice Fiscale: ");
+            string codiceFiscale = Console.ReadLine().ToUpper();
+            Console.Write("Reddito annuale: ");
+            double redditoAnnuale = double.Parse(Console.ReadLine());
+
+            // Creazione di un oggetto Contribuente con i dati inseriti dall'utente
+            Contribuente contribuente = new Contribuente(nome, cognome, genere, dataNascita, comuneResidenza, redditoAnnuale, codiceFiscale);
+
+            // Output dei risultati del calcolo dell'imposta
+            Console.WriteLine("====================================");
+            Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE");
+            Console.WriteLine($"Contribuente: {contribuente.nome.ToUpper()} {contribuente.cognome.ToUpper()}");
+            Console.WriteLine($"nato il {contribuente.dataNascita} ({contribuente.genere})");
+            Console.WriteLine($"Residente in {contribuente.comuneResidenza}");
+            Console.WriteLine($"Codice Fiscale: {contribuente.codiceFiscale}");
+            Console.WriteLine($"Reddito Dichiarato:  € {contribuente.redditoAnnuale:N2}");
+            Console.WriteLine($"Imposta da versare: € {contribuente.CalcoloImposta():N2}");
+        }
+    }
+}
